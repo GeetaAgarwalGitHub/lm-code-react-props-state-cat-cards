da4f588fedee55a167c96889c232c7a4fd179a8b
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-09 20:55:14 +0100

    Update cat_card.tsx
    Activity 5

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,9 +1,29 @@
-const CatCard : React.FC = () =>
+
+interface CatCardProps{
+    name: string;
+    species: string;
+    favFoods: Array<string>;
+    birthYear: number;
+ }
+
+ interface CatObject {
+     cat : CatCardProps;
+ }
+
+ const CatCardUsingProps : React.FC <CatCardProps>= (prop) =>
         <div className="card">
-            <h3 className="card__text card__header">Mr Gwumpy</h3>
-            <p className="card__text">Species: Cat</p>
-            <p className="card__text">Favourite Food(s): Caviar</p>
-            <p className="card__text">Birth Year: 1984</p>
+            <h3 className="card__text card__header">{prop.name}</h3>
+            <p className="card__text">Species: {prop.species}</p>
+            <p className="card__text">Favourite Food(s): {prop.favFoods}</p>
+            <p className="card__text">Birth Year: {prop.birthYear}</p>
         </div>;
 
-export default CatCard;
+ const CatCardUsingObject : React.FC <CatObject>= (catObject) =>
+        <div className="card">
+            <h3 className="card__text card__header">{catObject.cat.name}</h3>
+            <p className="card__text">Species: {catObject.cat.species}</p>
+            <p className="card__text">Favourite Food(s): {catObject.cat.favFoods}</p>
+            <p className="card__text">Birth Year: {catObject.cat.birthYear}</p>
+        </div>;
+
+export default { CatCardUsingObject, CatCardUsingProps };
