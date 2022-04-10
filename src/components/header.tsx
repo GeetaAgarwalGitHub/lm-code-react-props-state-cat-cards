2bd374d0008ec1a9bc4b47c02052921e7b5e25f1
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-10 01:36:54 +0100

    Update header.tsx
    Activity 8

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,16 @@ interface HeaderProp {
 	NumberOfElements:number;
 }
 
-const Header: React.FC <HeaderProp> = (props) => (
+const Header: React.FC <HeaderProp> = (props) => {
+	const {NumberOfElements} = props
+return(	
 	<header className='header__container'>
 		<h1 className='header__title header__welcome'>Welcome to React!</h1>
 		<h2 className='header__title'>
-			There are currently {props.NumberOfElements} Cats in this Cat App
+			There are currently {NumberOfElements} Cats in this Cat App
 		</h2>
 	</header>
-);
+	);
+}
 
 export default Header;
