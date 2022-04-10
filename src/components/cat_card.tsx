1fc081260701eab9c74fbed52d35b6f5c237b6fc
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-10 01:36:38 +0100

    Update cat_card.tsx
    Activity 8

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,18 +1,7 @@
 import CatImage from "../components/cat_image";
-
+import  CatDataImages from "../data/catData"
  
-import cat1 from '../assets/images/cat1.jpg';
-import cat2 from '../assets/images/cat2.jpg';
-import cat3 from '../assets/images/cat3.jpg';
-import cat4 from '../assets/images/cat4.jpg';
-import cat5 from '../assets/images/cat5.jpg';
-import cat6 from '../assets/images/cat6.jpg';
-import cat7 from '../assets/images/cat7.jpg';
-import cat8 from '../assets/images/cat8.jpg';
-import cat9 from '../assets/images/cat9.jpg';
-import cat10 from '../assets/images/cat10.jpg';
-import cat11 from '../assets/images/cat11.jpg';
-import cat12 from '../assets/images/cat12.jpg';
+
 
 interface CatCardProps{
     name: string;
@@ -20,6 +9,7 @@ interface CatCardProps{
     favFoods: Array<string>;
     birthYear: number;
     catIndex : number;
+    id ?: string;
  }
 
 //  interface CatObject {
@@ -27,123 +17,32 @@ interface CatCardProps{
 //  }
 
 
- const images = [
-	{
-		image: cat1,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'amblin',
-		attributionUrl: 'https://www.flickr.com/people/amblin/'
-	},
-	{
-		image: cat2,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'ivva',
-		attributionUrl: 'https://www.flickr.com/people/ivva/'
-	},
-	{
-		image: cat3,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-ND 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
-		attributionName: 'Rachele Pettarelli Ph',
-		attributionUrl: 'https://www.flickr.com/people/rachephotos/'
-	},
-	{
-		image: cat4,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'renarl',
-		attributionUrl: 'https://www.flickr.com/people/renarl/'
-	},
-	{
-		image: cat5,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'x-oph',
-		attributionUrl: 'https://www.flickr.com/people/x-oph/'
-	},
-	{
-		image: cat6,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-ND 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
-		attributionName: 'arrathoonlaa@att.net',
-		attributionUrl: 'https://www.flickr.com/people/21851382@N04/'
-	},
-	{
-		image: cat7,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'pavlovskyy',
-		attributionUrl: 'https://www.flickr.com/people/pavlovskyy/'
-	},
-	{
-		image: cat8,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'AleGranholm',
-		attributionUrl: 'https://www.flickr.com/people/darthale/'
-	},
-	{
-		image: cat9,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'Sardonic G',
-		attributionUrl: 'https://www.flickr.com/people/24039825@N06/'
-	},
-	{
-		image: cat10,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by/2.0/',
-		attributionName: 'www.metaphoricalplatypus.com',
-		attributionUrl: 'https://www.flickr.com/people/29638108@N06/'
-	},
-	{
-		image: cat11,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'abraham.williams',
-		attributionUrl: 'https://www.flickr.com/people/4braham/'
-	},
-	{
-		image: cat12,
-		altText: 'Describe this cat!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'x-oph',
-		attributionUrl: 'https://www.flickr.com/people/x-oph/'
-	}
-];
-
- const CatCardUsingProps : React.FC <CatCardProps>= (prop, index) =>
+ const CatCardUsingProps : React.FC <CatCardProps>= (props) =>
+ {
+    const { name, species, favFoods, birthYear, catIndex, id } = props;
+   // const foundCatImage = CatDataImages.CatImages.find(image => image.id === id)
+    return (
+		
         <div className="card">
-            <h3 className="card__text card__header">{prop.name}</h3>
-            <p className="card__text">Species: {prop.species}</p>
-            <p className="card__text">Favourite Food(s): {prop.favFoods}</p>
-            <p className="card__text">Birth Year: {prop.birthYear}</p>
+            <h3 className="card__text card__header">{name}</h3>
+            <p className="card__text">Species: {species}</p>
+            <p className="card__text">Favourite Food(s): {favFoods}</p>
+            <p className="card__text">Birth Year: {birthYear}</p>
             
-            {prop.catIndex < images.length && (
+            {catIndex < CatDataImages.CatImages.length && (
 				<CatImage
-					img={images[prop.catIndex].image}
-					alt_Text={images[prop.catIndex].altText}
-					licenceType={images[prop.catIndex].licenceType}
-					licenceUrl={images[prop.catIndex].licenceUrl}
-					attributionName={images[prop.catIndex].attributionName}
-					attributionUrl={images[prop.catIndex].attributionUrl}
+					img={CatDataImages.CatImages[catIndex].img}
+					alt_Text={CatDataImages.CatImages[catIndex].alt_Text}
+					licenceType={CatDataImages.CatImages[catIndex].licenceType}
+					licenceUrl={CatDataImages.CatImages[catIndex].licenceUrl}
+					attributionName={CatDataImages.CatImages[catIndex].attributionName}
+					attributionUrl={CatDataImages.CatImages[catIndex].attributionUrl}
 				/>
 			)}
-        </div>;
+        </div>
+
+	);
+ };
 
 //  const CatCardUsingObject : React.FC <CatObject>= (catObject) =>
 //         <div className="card">
