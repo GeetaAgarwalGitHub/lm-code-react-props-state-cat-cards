1033e5c507a5e2ed24e9a2e9cd4dbb0f5cec4cc7
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-10 10:28:10 +0100

    Update header.tsx
    Activity 9

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
 interface HeaderProp {
 	NumberOfElements:number;
+	Animal :string;
 }
 
 const Header: React.FC <HeaderProp> = (props) => {
-	const {NumberOfElements} = props
+	const {NumberOfElements, Animal} = props
 return(	
 	<header className='header__container'>
 		<h1 className='header__title header__welcome'>Welcome to React!</h1>
 		<h2 className='header__title'>
-			There are currently {NumberOfElements} Cats in this Cat App
+			There are currently {NumberOfElements} {Animal} in this  App
 		</h2>
 	</header>
 	);
